319177ace84d6396783ba34283e3096f0c2d0c23
RicardoAlanEscobar <ra.gomez@unineuuni.edu.mx>
2024-03-08 09:34:50 -0600

    Updates

--- fr/assets/js/814f3328.635f4a13.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[2535],{5641:e=>{e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Cursos","permalink":"/fr/blog/2024/04/29/","unlisted":false},{"title":"prueba 2","permalink":"/fr/blog/2024/03/10/","unlisted":false},{"title":"ded","permalink":"/fr/blog/ded","unlisted":false}]}')}}]);
