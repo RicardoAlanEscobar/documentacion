5a5d696b74f143dc2aedae10f34b2946595117ef
RicardoAlanEscobar <ra.gomez@unineuuni.edu.mx>
2024-03-15 13:03:39 -0600

    Updates

--- assets/js/c77d9189.ed300cd9.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[213],{7624:e=>{e.exports=JSON.parse('{"permalink":"/documentacion/index.html/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":3,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
